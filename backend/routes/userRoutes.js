b092224771e2f6922cd542e87aea1f1c382387e8
Dhananjay Puranik <dhananjaypuranik@192.168.1.6>
2024-03-15 16:31:02 +0530

    added chat controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const {registerUser,authUser,allUsers}=require("../controllers/userControllers");
+const {protect}=require('../middleware/authMiddleware');
 const router=express.Router();
 
-router.route('/').post(registerUser).get(allUsers);
+router.route('/').post(registerUser).get(protect,allUsers);
 router.route('/login',authUser);
  
 
-module.exports=router;0
+module.exports=router;
